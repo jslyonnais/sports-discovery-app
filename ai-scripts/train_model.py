fa3839a21809515018ff5d6a33fc85f6f8f66aef
hugo.tremblay <hugo.tremblay@gsoft.com>
2019-03-29 13:27:43 -0400

    Update ai scfripts

--- ai-scripts/train_model.py
+++ ai-scripts/train_model.py
@@ -1,15 +1,18 @@
 import pandas as pd 
 import numpy as np
+import datetime
 import keras
 from keras.models import Sequential
-from keras.datasets import mnist
+from keras.models import load_model
 from keras.layers.core import Dense, Dropout, Activation
 from sklearn.model_selection import train_test_split
 
-batch_size = 5
-epochs = 1
+batch_size = 100
+epochs = 100
+ts = str(datetime.datetime.now().timestamp())
+model_file_name = 'model-' + ts + '.h5'
 
-path = r'C:\Dev\sports-discovery-app\ai-scripts\data-formated.csv'
+path = r'C:\Dev\sports-discovery-app\ai-scripts\fake-data-formated.csv'
 df = pd.read_csv(path)
 df = df.drop('Unnamed: 0', axis=1)
 
@@ -23,6 +26,9 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 
 nb_sports = len(y.columns)
 nb_features = len(X.columns)
+
+checkpoint = keras.callbacks.ModelCheckpoint(model_file_name, verbose=1, monitor='val_loss',save_best_only=True, mode='auto')  
+
 
 model = Sequential()
 model.add(Dense(512, input_shape=(nb_features,)))
@@ -42,15 +48,23 @@ model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
+          callbacks=[checkpoint],
           validation_data=(x_test, y_test))
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
-model.save('model.h5')  # creates a HDF5 file 'my_model.h5'
-del model  # deletes the existing model
+model = load_model(model_file_name)
 
-# returns a compiled model
-# identical to the previous one
-model = load_model('model.h5')
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+h = X[0:1].copy()
+
+prediction = model.predict(h)
+results = pd.DataFrame(data=prediction, columns=y.columns).T.sort_values(by=[0], ascending=False)
+results['sports'] = results.index
+results
+print(results)
