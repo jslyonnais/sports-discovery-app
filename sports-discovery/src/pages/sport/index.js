625e3f34d77b00f5495923fed0476b676a9b3484
jslyonnais <iam@jslyonnais.com>
2019-03-29 17:17:26 -0400

    adapt text

--- sports-discovery/src/pages/sport/index.js
+++ sports-discovery/src/pages/sport/index.js
@@ -135,7 +135,7 @@ export class SportPage extends Component {
                     <img className={classes.overlayImage} src={sport.image}/>
                     <div className={classes.overlayContainer}>
                         <h1 className={classes.overlayTitle}>{sport.title}</h1>
-                        <div className={classes.overlayDesc}>Based on your the sports you've entered, <br/>we've supposed you'll like to play {sport.title}.</div>
+                        <div className={classes.overlayDesc}>Based on your choices of sports, <br/>our genius AI have find that you would like to play to {sport.title}.</div>
                     </div>
                 </div>
                 <div className={classes.rightSideSection}>
