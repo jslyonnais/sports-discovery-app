850ae49e0a5aa5503700c2ee88a6c5d3c65ada47
jslyonnais <iam@jslyonnais.com>
2019-03-29 17:59:41 -0400

    styling location page

--- sports-discovery/src/pages/sport/location.js
+++ sports-discovery/src/pages/sport/location.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 
+import { sportsData } from './sportsData.js'
 import GoogleMapReact from 'google-map-react';
+import Icon from '@material-ui/core/Icon';
 import LocationIcon from '@material-ui/icons/LocationOn';
 import Typography from '@material-ui/core/Typography';
-import { sportsData } from './sportsData.js'
 import withStyles from '@material-ui/core/styles/withStyles';
 
 const styles = theme => ({
     paper: {
         marginTop: theme.spacing.unit * 6,
         marginBottom: theme.spacing.unit * 6,
-        padding: theme.spacing.unit * 14
+        padding: theme.spacing.unit * 14,
+        textAlign: 'center'
     },
     title: {
         display: 'block',
         color: '#4f4f4f',
         fontWeight: 700,
         marginBottom: theme.spacing.unit * 6
+    },
+    sub: {
+        display: 'block',
+        fontSize: 18,
+        color: '#bbb'
     },
     leftSideSection: {
         flex: 1,
@@ -36,7 +43,7 @@ const styles = theme => ({
     },
     overlayContainer: {
         width: '80%',
-        textAlign: 'center',
+        textAlign: 'left',
         zIndex: 1
     },
     overlayTitle: {
@@ -72,17 +79,6 @@ const styles = theme => ({
     gearTitle: {
         fontSize: 12,
         color: '#BBB'
-    },
-    link: {
-        color: '#48bbff',
-        textDecoration: 'underline',
-        fontWeight: 700,
-        display: 'inline-block',
-        marginLeft: theme.spacing.unit,
-        paddingLeft: theme.spacing.unit,
-        borderLeft: '1px solid #e0e0e0',
-        cursor: 'pointer',
-        lineHeight: 1
     },
     location: {
         width: '100%',
@@ -120,6 +116,38 @@ const styles = theme => ({
         display: 'flex',
         alignItems: 'center'
     },
+    locationList: {
+
+    },
+    location: {
+        textAlign: 'left',
+        borderBottom: '1px solid #e0e0e0',
+        paddingBottom: theme.spacing.unit*2,
+        paddingTop: theme.spacing.unit *2,
+        display: 'flex',
+        '&:last-child': {
+            border: 0
+        }
+    },
+    locationTitle: {
+        display: 'block',
+        fontSize: 20,
+        fontWeight: 700,
+        marginBottom: theme.spacing.unit
+    },
+    locationAddress: {
+
+    },
+    locationContainer: {
+        marginLeft: theme.spacing.unit
+    },
+    locationLink: {
+        display: 'block',
+        color: '#48bbff',
+        textDecoration: 'underline',
+        fontWeight: 700,
+        cursor: 'pointer',
+    }
 });
 
 export class SportLocationPage extends Component {
@@ -154,7 +182,7 @@ export class SportLocationPage extends Component {
         return (
             <>
                 <div className={classes.leftSideSection}>
-                    <div style={{ height: '100vh', width: '100%' }}>
+                    <div style={{ height: '100vh', width: '100%', position: 'absolute', top: 0 }}>
                         <GoogleMapReact
                             bootstrapURLKeys={{ key: "AIzaSyADZ6SKjElEyIdZ7og8PzLEBZ6zLOAtPz8" }}
                             defaultCenter={location}
@@ -169,13 +197,57 @@ export class SportLocationPage extends Component {
                     <div className={classes.paper}>
                         <Typography 
                             component="h2"
-                            variant="h3" 
-                            align="center"
+                            variant="h4" 
+                            align="left"
                             className={classes.title}
                             gutterBottom
                         >
-                            Try {sport.title}
+                            <span className={classes.sub}>Location near</span> Montreal, QC, Canada
                         </Typography>
+
+
+                        <div className={classes.locationList}>
+                            <div className={classes.location}>
+                                <Icon className={classes.icon}>
+                                    location_on
+                                </Icon>
+                                <div className={classes.locationContainer}>
+                                    <span className={classes.locationTitle}>Lorem Ipsum</span>
+                                    <span className={classes.locationAddress}>1700 Saint-Patrick St, Montreal, QC H3K 1A7</span>
+                                    <span className={classes.locationLink}>Get directions</span>
+                                </div>
+                            </div>
+                            <div className={classes.location}>
+                                <Icon className={classes.icon}>
+                                    location_on
+                                </Icon>
+                                <div className={classes.locationContainer}>
+                                    <span className={classes.locationTitle}>Lorem Ipsum</span>
+                                    <span className={classes.locationAddress}>1700 Saint-Patrick St, Montreal, QC H3K 1A7</span>
+                                    <span className={classes.locationLink}>Get directions</span>
+                                </div>
+                            </div>
+                            <div className={classes.location}>
+                                <Icon className={classes.icon}>
+                                    location_on
+                                </Icon>
+                                <div className={classes.locationContainer}>
+                                    <span className={classes.locationTitle}>Lorem Ipsum</span>
+                                    <span className={classes.locationAddress}>1700 Saint-Patrick St, Montreal, QC H3K 1A7</span>
+                                    <span className={classes.locationLink}>Get directions</span>
+                                </div>
+                            </div>
+                            <div className={classes.location}>
+                                <Icon className={classes.icon}>
+                                    location_on
+                                </Icon>
+                                <div className={classes.locationContainer}>
+                                    <span className={classes.locationTitle}>Lorem Ipsum</span>
+                                    <span className={classes.locationAddress}>1700 Saint-Patrick St, Montreal, QC H3K 1A7</span>
+                                    <span className={classes.locationLink}>Get directions</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </>
