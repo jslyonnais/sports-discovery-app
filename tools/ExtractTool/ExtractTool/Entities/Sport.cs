412f5016d45e2ba3efec1b17f873804d0beb79c6
Joseph Passineau <joseph.passineau@gsoft.com>
2019-03-29 09:38:23 -0400

    Init extraction tool for the sports API

---
+++ tools/ExtractTool/ExtractTool/Entities/Sport.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtractTool.Entities
+{
+    public class Sport
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("attributes")]
+        public SportAttributes Attributes { get; set; }
+
+        public bool IsParent => this.Attributes?.ParentId != null;
+    }
+}
